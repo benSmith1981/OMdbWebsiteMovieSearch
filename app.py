73f8054f088aeba90eb0a383d91c4ec9149db5cd
Ben Smith <benjaminsmith1981@googlemail.com>
2023-03-30 09:04:59 +0000

    Setup project

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request 
+
+import requests 
+
+
+app = Flask(__name__) 
+
+
+@app.route('/', methods=['GET', 'POST']) 
+
+def index(): 
+
+    movies = [] 
+ 
+
+    if request.method == 'POST': 
+
+        #this gets the search query text from the input box on the webpage and stores it in 
+        #a local variable called search_query
+        search_query = request.form['search_query'] 
+
+        #this is an api key that allows us to search OMBD
+        api_key = "f0674f3f" 
+
+        url = f"http://www.omdbapi.com/?s={search_query}&apikey={api_key}" 
+
+        response = requests.get(url) 
+
+        data = response.json() 
+
+        print(data) #look in the terminal at the data 
+    return render_template('index.html', movies=movies) 
+
+if __name__ == "__main__": 
+
+    app.run(debug=True) 
